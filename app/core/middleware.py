019a4f051205145187aca0ea5c9730527c094251
Charly Ay <Charly1986-ay@users.noreply.github.com>
2026-04-03 11:27:52 -0300

    actualizacion de middleware

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -1,9 +1,12 @@
 
 import time
+import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from h11 import Request
 
+#BLACKLIST = {'127.0.0.1'}
+BLACKLIST = {}
 
 def register_middleware(app: FastAPI):
 
@@ -14,3 +17,26 @@ def register_middleware(app: FastAPI):
         process_time = time.perf_counter() - start
         response.headers['X-Process-Time'] = f'{process_time:.4f} s'
         return response
+    
+    @app.middleware('http')
+    async def log_request(request: Request, call_next):
+        print(f'**ENTRADA: {request.method} {request.url}**')
+        response = await call_next(request)
+        print(f'**SALIDA: {response.status_code}**')
+        return response
+    
+    @app.middleware('http')
+    async def add_process_id_header(request: Request, call_next):
+        request_id = str(uuid.uuid4())
+        response = await call_next(request)
+        response.headers['X-request-ID'] = request_id
+        return response
+    
+    @app.middleware('http')
+    async def block_ip_middleware(request: Request, call_next):
+        cliente_ip = request.client.host
+        if cliente_ip in BLACKLIST: raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, 
+            detail='Acesso denegado a esa IP'
+        )
+        return await call_next(request)        
